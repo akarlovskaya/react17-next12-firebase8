2d886cd3a27967c1fb0a7024f5270d0e433cd6cc
Anna Karlovskaya <annakarlovsk@gmail.com>
2024-12-07 20:54:22 -0800

    Customizing Metatags component

--- components/Metatags.js
+++ components/Metatags.js
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 
 export default function Metatags({
-  title = 'The Full Next.js + Firebase Course',
-  description = 'A complete Next.js + Firebase course by Fireship.io',
-  image = 'https://fireship.io/courses/react-next-firebase/img/featured.png',
+  title = 'Vanklas - Discover Fitness Drop-in Classes',
+  description = 'Step into journey of transforming physically and mentally',
+  image = 'vanklasFavicon.png',
 }) {
   return (
     <Head>
--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Metatags from '../../components/Metatags';
 
 function AdminPostEdit() {
   return (
+    <>
+    <Metatags title="Admin Page" description="Sign up for this amazing app!" />
     <div>
       Edit Post
     </div>
+    </>
+
   )
 }
 
